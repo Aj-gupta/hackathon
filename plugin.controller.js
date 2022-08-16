57042dd8c2eab20c4ac625e855aa3dd1dc8cf76d
Ajay Kumar Gupta <gajay3954@gmail.com>
2022-08-17 02:29:24 +0530

    add: youtube download

--- plugin.controller.js
+++ plugin.controller.js
@@ -1,4 +1,5 @@
 import { createRequire } from "module";
+import fs from "fs";
 const require = createRequire(import.meta.url);
 const { Deepgram } = require("@deepgram/sdk");
 const YoutubeMp3Downloader = require("youtube-mp3-downloader");
@@ -7,25 +8,70 @@ const ffmpeg = require("ffmpeg-static");
 export async function getSubtitle(req, res) {
   try {
     let url = req.query.url;
+
     if (!url) {
       return res.status(400).json({ message: "url is not defined" });
     }
-    if (url.indexOf("youtube.com") != -1) {
-      console.log("youtube url");
-    }
-    const YD = new YoutubeMp3Downloader({
-      ffmpegPath: ffmpeg,
-      outputPath: "./",
-      youtubeVideoQuality: "highestaudio",
-    });
 
-    const deepgram = new Deepgram(process.env.DEEPGRAM_SECRET);
-    const response = await deepgram.transcription.preRecorded(
-      {
-        url: req.query.url,
-      },
-      { punctuate: true, utterances: true }
-    );
+    let youtubeIndex = url.indexOf("?v=");
+
+    if (youtubeIndex != -1) {
+      const YD = new YoutubeMp3Downloader({
+        ffmpegPath: ffmpeg,
+        outputPath: "./",
+        youtubeVideoQuality: "highestaudio",
+      });
+
+      const deepgram = new Deepgram(process.env.DEEPGRAM_SECRET);
+
+      YD.download(url.substr(youtubeIndex + 3));
+
+      YD.on("progress", (data) => {
+        console.log(data.progress.percentage + "% downloaded");
+      });
+
+      YD.on("finished", async (err, video) => {
+        if (err) {
+          return res.status(400).json({ message: err.message });
+        }
+        const videoFileName = video.file;
+        console.log(`Downloaded ${videoFileName}`);
+
+        const response = await deepgram.transcription.preRecorded(
+          {
+            buffer: fs.readFileSync(videoFileName),
+            mimetype: "audio/mp3",
+          },
+          { punctuate: true, utterances: true }
+        );
+        res.writeHead(200, {
+          "Content-Type": "text/plain",
+          "Content-disposition": "attachment;filename=" + "subtitle.vtt",
+          "Content-Length": response.toWebVTT().length,
+        });
+        fs.unlinkSync(videoFileName);
+
+        return res.end(Buffer.from(response.toWebVTT(), "binary"));
+        // return res.json({ message: "success" });
+      });
+
+      YD.on("error", (err) => {
+        return res.status(400).json({ message: err.message || "error" });
+      });
+    } else {
+      const response = await deepgram.transcription.preRecorded(
+        {
+          url: url,
+        },
+        { punctuate: true, utterances: true }
+      );
+      res.writeHead(200, {
+        "Content-Type": "text/plain",
+        "Content-disposition": "attachment;filename=" + "subtitle.vtt",
+        "Content-Length": response.toWebVTT().length,
+      });
+      return res.end(Buffer.from(response.toWebVTT(), "binary"));
+    }
 
     // const stream = fs.createWriteStream("output.vtt", { flags: "a" });
 
@@ -36,9 +82,8 @@ export async function getSubtitle(req, res) {
     //   "Content-Length": response.toWebVTT().length,
     // });
     // return res.end(Buffer.from(response.toWebVTT(), "binary"));
-    return res.json({ message: "success" });
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ message: err.message });
+    return res.status(400).json({ message: err.message || "Server error" });
   }
 }
